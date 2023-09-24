8b39a1a185960e4d31eab1666acd8892197d6a25
swathi <uttaralaswathi1998@gmail.com>
2023-09-24 02:33:54 +0000

    42

---
+++ src/components/Home/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+import Navbar from '../Navbar'
+import './index.css'
+
+const Home = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+      const homeBgClassName = isDarkTheme ? 'home-bg-dark' : 'home-bg-light'
+
+      const homeImageClassName = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+
+      const homeTextClassName = isDarkTheme
+        ? 'home-text-light'
+        : 'home-text-dark'
+
+      return (
+        <div className={`home-app-container ${homeBgClassName}`}>
+          <Navbar />
+          <div className="home-page-container">
+            <div className="home-container">
+              <img className="home-image" src={homeImageClassName} alt="home" />
+              <h1 className={`home-heading ${homeTextClassName}`}>About</h1>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+export default Home
